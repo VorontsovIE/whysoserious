c6b0c2c5b97d22bb2e1edb0113643a60fd006db4
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2018-01-21 01:09:40 +0300

    refine code a bit

--- catboost_data_and_labels_separate.py
+++ catboost_data_and_labels_separate.py
@@ -1,4 +1,4 @@
-import numpy 
+import numpy
 import pandas as pd
 from catboost import CatBoostRegressor
 
@@ -14,7 +14,29 @@ def print_list(file, arr):
 
 train_data = pd.read_csv('/home/ilya/programming/whysoserioushack/train_data_subset.tsv', sep='\t', dtype=str).astype(str)
 train_labels = pd.read_csv('/home/ilya/programming/whysoserioushack/train_labels.tsv')['risk_category']
-cat_features = [i for i in range(len(train_data.keys()))]
+
+#  1  region
+#  2  company_type
+#  3  okved_osn_code
+#  4  init_equities
+#  5  init_equity_types
+#  6  purpose
+#  7  other_reasons_for_check
+#  8  day_length
+#  9  hour_length
+# 10  type
+# 11  agency
+# 12  city
+# 13  type_nasel_punkt
+# 14  month
+# 15  day
+# 16  weekday
+# 17  yearday
+# 18  osn_1
+# 19  osn_1_2
+# 20  isbudget
+
+cat_features = [0,1,2,4,5,6,9,10,11,12,17,18] #[i for i in range(len(train_data.keys()))]
 
 model = CatBoostRegressor(learning_rate=1, depth=6, loss_function='RMSE')
 fit_model = model.fit(train_data, train_labels, cat_features=cat_features)
